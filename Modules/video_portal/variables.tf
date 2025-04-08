9ed27f81c789ed4f69d434461eadb0d594703622
Vince Marando <Vince.Marando@georgebrown.ca>
2025-04-08 13:38:11 -0400

    Fix user_data script with templatefile function to inject variables

--- Modules/video_portal/variables.tf
+++ Modules/video_portal/variables.tf
@@ -1,4 +1,8 @@
 variable "vpc_id" {}
 variable "public_subnet_id" {}
 variable "key_name" {}
-variable "bucket_name" {}
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
