e1c160987f3598c10cfe55d793095095d4fee1c5
Vince Marando <Vince.Marando@georgebrown.ca>
2025-04-08 13:44:51 -0400

    Fix shell array access for video files in user_data section

--- Modules/video_portal/main.tf
+++ Modules/video_portal/main.tf
@@ -1,62 +1,35 @@
-resource "aws_security_group" "web_sg" {
-  name        = "web-portal-sg"
-  description = "Allow web traffic"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "web" {
-  ami                    = "ami-0c02fb55956c7d316"
-  instance_type           = "t2.micro"
-  key_name                = var.key_name
-  subnet_id              = var.public_subnet_id
+  ami             = "ami-0c02fb55956c7d316"
+  instance_type   = "t2.micro"
+  key_name        = var.key_name
+  subnet_id       = var.public_subnet_id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y nginx aws-cli
-    systemctl enable nginx
-    systemctl start nginx
+  #!/bin/bash
+  yum update -y
+  yum install -y nginx aws-cli
+  systemctl enable nginx
+  systemctl start nginx
 
-    BUCKET_URL="https://${var.bucket_name}.s3.amazonaws.com"
-    # List of video files you want to display dynamically
-    VIDEO_FILES=("video1.mp4" "video2.mp4" "video3.mp4" "video4.mp4")
+  BUCKET_URL="https://${var.bucket_name}.s3.amazonaws.com"
+  # List of video files you want to display dynamically
+  VIDEO_FILES=("video1.mp4" "video2.mp4" "video3.mp4" "video4.mp4")
 
-    echo "<html><body><h1>Client Video Footage</h1><ul>" > /usr/share/nginx/html/index.html
+  echo "<html><body><h1>Client Video Footage</h1><ul>" > /usr/share/nginx/html/index.html
 
-    # Check if there are video files to display
-    if [ ${#VIDEO_FILES[@]} -eq 0 ]; then
-      echo "<p>No videos available at this time.</p>" >> /usr/share/nginx/html/index.html
-    else
-      # Loop through the array of videos and generate the <li> tags
-      for video in "${VIDEO_FILES[@]}"
-      do
-        echo "<li><a href='${BUCKET_URL}/$video'>$video</a></li>" >> /usr/share/nginx/html/index.html
-      done
-    fi
+  # Check if there are video files to display
+  if [ ${#VIDEO_FILES[@]} -eq 0 ]; then
+    echo "<p>No videos available at this time.</p>" >> /usr/share/nginx/html/index.html
+  else
+    # Loop through the array of videos and generate the <li> tags
+    for video in "${VIDEO_FILES[@]}"
+    do
+      echo "<li><a href='${BUCKET_URL}/$video'>$video</a></li>" >> /usr/share/nginx/html/index.html
+    done
+  fi
 
-    echo "</ul></body></html>" >> /usr/share/nginx/html/index.html
+  echo "</ul></body></html>" >> /usr/share/nginx/html/index.html
   EOF
 
   tags = {
