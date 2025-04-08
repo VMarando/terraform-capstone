33f37e6dd2f7b62a653b05b190b83f9178a1bd35
Vince Marando <Vince.Marando@georgebrown.ca>
2025-04-08 12:56:34 -0400

    Double escape Bash variable to prevent Terraform interpolation

--- Modules/video_portal/main.tf
+++ Modules/video_portal/main.tf
@@ -43,8 +43,8 @@ resource "aws_instance" "web" {
 
             echo "<html><body><h1>Client Video Footage</h1>" > /usr/share/nginx/html/index.html
             echo "<ul>" >> /usr/share/nginx/html/index.html
-            echo "<li><a href='\\${BUCKET_URL}/video1.mp4'>video1.mp4</a></li>" >> /usr/share/nginx/html/index.html
-            echo "<li><a href='\\${BUCKET_URL}/video2.mp4'>video2.mp4</a></li>" >> /usr/share/nginx/html/index.html
+            echo "<li><a href='\\\${BUCKET_URL}/video1.mp4'>video1.mp4</a></li>" >> /usr/share/nginx/html/index.html
+            echo "<li><a href='\\\${BUCKET_URL}/video2.mp4'>video2.mp4</a></li>" >> /usr/share/nginx/html/index.html
             echo "</ul></body></html>" >> /usr/share/nginx/html/index.html
             EOF
 
