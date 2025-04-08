04c3bbe51e0f1f36752b899401e88b8319c0cacf
Vince Marando <Vince.Marando@georgebrown.ca>
2025-04-08 15:34:03 -0400

    Update main.tf variables

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "video_portal" {
-  source = "./Modules/video_portal"  # Adjust to the correct path if necessary
-
-  # Pass the required variables
-  web_sg_id = aws_security_group.web_sg.id   # Assuming web_sg is created in the parent configuration
-  vpc_id    = var.vpc_id                     # This should be defined in your variables.tf or elsewhere
+  source            = "./video_portal"
+  bucket_name       = var.bucket_name
+  key_name          = var.key_name
+  public_subnet_id  = var.public_subnet_id
+  vpc_id            = var.vpc_id
+  web_sg_id         = var.web_sg_id
 }
-
 
