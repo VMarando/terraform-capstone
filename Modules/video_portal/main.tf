5ad69a98ff7ffad538127bc085a6de8a29e2f922
Vince Marando <Vince.Marando@georgebrown.ca>
2025-04-08 13:48:10 -0400

    Fix Terraform configuration for AWS instance with user_data script

--- Modules/video_portal/main.tf
+++ Modules/video_portal/main.tf
@@ -1,26 +1,38 @@
-user_data = <<-EOF
-#!/bin/bash
-yum update -y
-yum install -y nginx aws-cli
-systemctl enable nginx
-systemctl start nginx
+resource "aws_instance" "web" {
+  ami             = "ami-0c02fb55956c7d316"
+  instance_type   = "t2.micro"
+  key_name        = var.key_name
+  subnet_id       = var.public_subnet_id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
-BUCKET_URL="https://${var.bucket_name}.s3.amazonaws.com"
-# List of video files you want to display dynamically
-VIDEO_FILES=("video1.mp4" "video2.mp4" "video3.mp4" "video4.mp4")
+  user_data = <<-EOF
+  #!/bin/bash
+  yum update -y
+  yum install -y nginx aws-cli
+  systemctl enable nginx
+  systemctl start nginx
 
-echo "<html><body><h1>Client Video Footage</h1><ul>" > /usr/share/nginx/html/index.html
+  BUCKET_URL="https://${var.bucket_name}.s3.amazonaws.com"
+  # List of video files you want to display dynamically
+  VIDEO_FILES=("video1.mp4" "video2.mp4" "video3.mp4" "video4.mp4")
 
-# Check if there are video files to display
-if [ \${#VIDEO_FILES[@]} -eq 0 ]; then
-  echo "<p>No videos available at this time.</p>" >> /usr/share/nginx/html/index.html
-else
-  # Loop through the array of videos and generate the <li> tags
-  for video in "\${VIDEO_FILES[@]}"
-  do
-    echo "<li><a href='\${BUCKET_URL}/\$video'>\$video</a></li>" >> /usr/share/nginx/html/index.html
-  done
-fi
+  echo "<html><body><h1>Client Video Footage</h1><ul>" > /usr/share/nginx/html/index.html
 
-echo "</ul></body></html>" >> /usr/share/nginx/html/index.html
-EOF
+  # Check if there are video files to display
+  if [ \${#VIDEO_FILES[@]} -eq 0 ]; then
+    echo "<p>No videos available at this time.</p>" >> /usr/share/nginx/html/index.html
+  else
+    # Loop through the array of videos and generate the <li> tags
+    for video in "\${VIDEO_FILES[@]}"
+    do
+      echo "<li><a href='\${BUCKET_URL}/\$video'>\$video</a></li>" >> /usr/share/nginx/html/index.html
+    done
+  fi
+
+  echo "</ul></body></html>" >> /usr/share/nginx/html/index.html
+  EOF
+
+  tags = {
+    Name = "VideoWebServer"
+  }
+}
