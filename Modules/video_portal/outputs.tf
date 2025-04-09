55c9f2ab2b5ceda615773aa8dccc880222260752
Vince Marando <Vince.Marando@georgebrown.ca>
2025-04-09 18:45:02 -0400

    Add output for random _id

--- Modules/video_portal/outputs.tf
+++ Modules/video_portal/outputs.tf
@@ -11,3 +11,8 @@ output "ftp_s3_sync_server_public_ip" {
   description = "Public IP address of the FTP-to-S3 sync server."
   value       = aws_instance.ftp_s3_sync_server.public_ip
 }
+
+output "random_id" {
+  value = random_id.common_id.hex
+  description = "The unique random ID generated for tenant isolation"
+}
