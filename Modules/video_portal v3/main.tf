891248f47e07396d6506d3c98c70779fc1a71123
Vince Marando <Vince.Marando@georgebrown.ca>
2025-04-10 21:48:06 -0400

    Change location / in the https configuration

--- Modules/video_portal v3/main.tf
+++ Modules/video_portal v3/main.tf
@@ -240,7 +240,7 @@ server {
     ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
 
     location / {
-        try_files \$uri \$uri/ =404;
+        try_files $uri $uri/ /index.html;
     }
 }
 NGINX_EOF
